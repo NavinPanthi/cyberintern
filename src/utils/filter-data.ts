93c74cdd1abff6fb0155ca2294289a5b38a1f7d7
NavinPanthi <panthinabin341@gmail.com>
2025-10-30 12:59:54 +0000

    Add utils

---
+++ src/utils/filter-data.ts
@@ -0,0 +1,20 @@
+export const conditions = [
+  { id: "brand_new", title: "Brand New" },
+  { id: "like_new", title: "Like New" },
+  { id: "very_good", title: "Very Good" },
+  { id: "good", title: "Good" },
+  { id: "acceptable", title: "Acceptable" },
+  { id: "damaged", title: "Damaged" },
+];
+export const verifiedOptions = [
+  { id: "all", value: "", name: "All" },
+  { id: "verified", value: "true", name: "Verified" },
+  { id: "non-verified", value: "false", name: "Non Verified" },
+];
+
+export const orderStatusOptions = [
+  { id: "all", value: "", name: "All" },
+  { id: "pending", value: "pending", name: "Pending" },
+  { id: "completed", value: "completed", name: "Completed" },
+  { id: "cancelled", value: "cancelled", name: "Cancelled" },
+];
